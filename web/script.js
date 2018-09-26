38b235e76848535dcf9d842d446d1ff3ae7821c5
Vanskater362 <wag12011@byui.edu>
2018-09-25 22:12:13 -0500

    adding week 2 files

---
+++ web/script.js
@@ -0,0 +1,12 @@
+function clicked() {
+   alert("Button Clicked!");
+}
+
+function mouseOver(id) {
+   document.getElementById(id).style.fontWeight = "bold";
+}
+
+function mouseOut(id) {
+   document.getElementById(id).style.fontWeight = "normal";
+}
+
