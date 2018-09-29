354dc0c1eb5e8f4323814d3ef20cadab9b0c0bd9
Vanskater362 <wag12011@byui.edu>
2018-09-29 13:03:06 -0500

    changed index.php

--- web/index.php
+++ web/index.php
@@ -20,7 +20,7 @@ print("
 
     <div class=\"container\">
       <div class=\"masthead\">
-        <h3 class=\"text-muted\">Anthony Wagner\'s Personal Page</h3>
+        <h3 class=\"text-muted\">Anthony Wagner's Personal Page</h3>
        <nav>
           <ul class=\"nav nav-justified\">
             <li class=\"active\"><a href=\"index.html\">Home</a></li>
@@ -45,14 +45,19 @@ print("
 
       <div class=\"row\">
         <div class=\"col-lg-4\">
-          <h2>Safari bug warning!</h2>
-          <p class=\"text-danger\">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <h2>About Me</h2>
+          <p>I am currently enrolled in a computer programing degree at Brigham Young University - Idaho. I only have three semesters left before I finish!</p>
+          <p>I currently work full time as a coded systems designer in the Fire Alarm indurstry and I am going to school to make a career change to hopfully bring a better life for my family.</p>
 
         </div>
         <div class=\"col-lg-4\">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+          <h2>Hobbies</h2>
+          <ol>
+            <li>Cycling</li>
+            <li>Computer Gaming</li>
+            <li>Magic the Gathering</li>
+            <li>Automotive Work</li>
+          </ol>
 
        </div>
         <div class=\"col-lg-4\">    
