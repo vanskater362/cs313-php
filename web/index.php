4d9e793710ce223cbf1eb12d393f8e136966b249
Vanskater362 <wag12011@byui.edu>
2018-09-29 12:18:57 -0500

    changed index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,65 @@
-<?php
 
-  phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-?>
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Personal Homepage</title>
+  </head>
+
+  <body>
+
+    <div class="container">
+      <div class="masthead">
+        <h3 class="text-muted">Anthony Wagner's Personal Page</h3>
+        <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="assignments.html">Assignments</a></li>
+            <li><a href="content\files\Anthony Wagner Resume.pdf">Resume</a></li>
+            <li><a href="mailto:vanskater362@gmail.com">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="jumbotron">
+         <div class="row">
+            <div class="col-sm-4">
+               <img src="content\images\profile.jpg" class="img-circle"/>
+            </div>
+            <div class="col-md-8">
+               <h1>Anthony Wagner</h1>
+               <p class="lead">Husband, Father of two, Gamer, Athlete</p>
+            </div>
+         </div>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <h2>Safari bug warning!</h2>
+          <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+        </div>
+        <div class="col-lg-4">
+          <h2>Heading</h2>
+          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+
+       </div>
+        <div class="col-lg-4">    
+        <?php 
+            <h2>PHP Section</h2>
+            <p>echo date('l, F jS, Y');</p> 
+         ?>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
