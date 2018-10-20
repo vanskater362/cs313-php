6bc8f20bdb46e98fabe8e885238f07c93c9fb443
Vanskater362 <wag12011@byui.edu>
2018-10-20 18:57:21 -0500

    updated the select statement

--- web/joblist.php
+++ web/joblist.php
@@ -15,17 +15,17 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Scripture List</title>
+	<title>Job List</title>
 </head>
 
 <body>
 <div>
 
-<h1>Scripture Resources</h1>
+<h1>Job List</h1>
 
 <?php
 
-$statement = $db->prepare("SELECT id, name FROM job_name");
+$statement = $db->prepare("SELECT job_number.number, job_name.name FROM job_name INNER JOIN job_number ON job_number.id=job_name.id");
 $statement->execute();
 
 // Go through each result
@@ -35,7 +35,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 	// row, and we can access the different values based on their
 	// name
 	echo '<p>';
-	echo '<strong>' . $row['id'] . ' ' . $row['name'] . ':';
+	echo '<strong>' . $row['job_number.number'] . ' ' . $row['job_name.name'] . ':';
 	//echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
 	echo '</p>';
 }
