f4554030b15ad420d1d8dc3ae69c78425e48a110
Vanskater362 <wag12011@byui.edu>
2018-10-21 00:13:07 -0500

    added search from and button

--- web/joblist.php
+++ web/joblist.php
@@ -35,6 +35,7 @@ $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPass
             <li class="active"><a href="index.html">Home</a></li>
             <li><a href="assignments.html">Assignments</a></li>
             <li><button id="createBTN" onclick="showCreate()" type="button">Create New Job</button>
+            <li><button id="search" onclick="showSearch()" type="button">Search</button>
           </ul>
         </nav>
       </div>
@@ -56,10 +57,11 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 ?>
 </div>
 <!-- may need to move the location of this when the list gets longer -->
-<div id="createNew" style="background: lightgreen">
+<div class="row">
+<div class="col-lg-4" id="createNew" style="background: lightgreen">
    <h1>Create new Job</h1>
-   <br>
-   <form id="nform" method="post" action="#" style="display">
+   <hr>
+   <form id="nform" method="post" action="#">
       <p id="np">
       <label id="nlabel" placeholder="Job Number" for="number">Job Number: </label>
       <input id="ninput" type="text" name="jobNumber">
@@ -84,8 +86,40 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       <label id="nlabel" placeholder="76100" for="zip">Zip:</label>
       <input id="ninput" type="text" name="zip">
       </p>
-      <input type="submit" name="submit">
+      <input type="submit" name="Create New Job">
    </form>
+</div>
+<div class="col-lg-4" id="search" style="background: lightgreen">
+   <h1>Search Jobs</h1>
+   <hr>
+   <form id="nform" method="post" action="#">
+      <p id="np">
+      <label id="nlabel" placeholder="Job Number" for="number">Job Number: </label>
+      <input id="ninput" type="text" name="jobNumber">
+      </p>
+      <p id="np">
+      <label id="nlabel" placeholder="Job Name" for="name">Job Name:</label>
+      <input id="ninput" type="text" name="name">
+      </p>
+      <p id="np">
+      <label id="nlabel" placeholder="street" for="street">Street:</label>
+      <input id="ninput" type="text" name="street">
+      </p>
+      <p id="np">
+      <label id="nlabel" placeholder="City" for="city">City:</label>
+      <input id="ninput" type="text" name="city">
+      </p>
+      <p id="np">
+      <label id="nlabel" placeholder="Texas" for="State">State:</label>
+      <input id="ninput" type="text" name="state">
+      </p>
+      <p id="np">
+      <label id="nlabel" placeholder="76100" for="zip">Zip:</label>
+      <input id="ninput" type="text" name="zip">
+      </p>
+      <input type="submit" name="Search">
+   </form>
+</div>
 </div>
 </body>
 
