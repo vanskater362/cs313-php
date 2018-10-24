24d72474fa56d88e38410996b546292c3cc7a085
Vanskater362 <wag12011@byui.edu>
2018-10-23 21:59:20 -0500

    this time

--- web/Week 05/Group/insert.php
+++ web/Week 05/Group/insert.php
@@ -10,19 +10,6 @@ $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
 $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Scripture List</title>
-</head>
-
-<body>
-<div>
-
-<h1>Scripture Resources</h1>
-
-<?php
 
 $book = $_POST['bookText'];
 $chapter = $_POST['chapText'];
@@ -30,7 +17,7 @@ $verse = $_POST['verseText'];
 $content = $_POST['contentText'];
 $topics = $_POST['topics'];
 
-$statement = $db->prepare("INSERT INTO scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content");
+$statement = $db->prepare('INSERT INTO scriptures (book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content');
 
 $statement->bindValue(':book', $book);
 $statement->bindValue(':chapter', $chapter);
@@ -38,14 +25,5 @@ $statement->bindValue(':verse', $verse);
 $statement->bindValue(':content', $content);
 $statement->execute();
 
-$statement = $db->prepare("SELECT book, chapter, verse, content FROM scriptures");
-$statement->execute();
-
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-	echo '<p>';
-	echo '<strong>' . $row['book'] . ' ' . $row['chapter'] . ':';
-	echo $row['verse'] . '</strong>' . ' - ' . $row['content'];
-	echo '</p>';
-}
+header("Location :showTables.php");
 ?>
