d8a606c21ae4b2b76092ffab60eeb4df6a1618a6
Vanskater362 <wag12011@byui.edu>
2018-10-25 21:31:38 -0500

    adding details page

---
+++ web/details.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"/>   
+    <link rel="stylesheet" href="styles.css"/> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
+	<title>Job Details</title>
+</head>
+
+<body>
+
+<div class="container">
+      <div class="masthead">
+        <h3 class="text-muted">Job Details</h3>
+       <nav>
+          <ul class="nav nav-justified">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="assignments.html">Assignments</a></li>
+            <li><a href="joblist.php">Job List</a></li>
+          </ul>
+        </nav>
+      </div>
+<div>
+
+<?php
+require("dbConnect.php");
+$db = get_db();
+
+session_start();
+echo $_SESSION['jobnumberId'];
+?>
--- web/joblist.php
+++ web/joblist.php
@@ -33,14 +33,16 @@
 <?php
 require("db/dbConnect.php");
 $db = get_db();
+session_start();
 
 $statement = $db->prepare("SELECT job_number.number, job_name.name FROM job_name INNER JOIN job_number ON job_number.name=job_name.id");
 $statement->execute();
 
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
+   $_SESSION['jobnumberID'] = $row['job_number.id'];
    echo '<ul>';
-	echo '<li id="list"><strong>' . $row['number'] . ' ' . $row['name'] . ' ' . '<a href="details.php">Details Page</a>';
+	echo '<li id="list"><strong>' . $row['number'] . ' ' . $row['name'] . ' ' . '<a href="details.php">Details</a>';
    echo '</ul><hr>';
 }
 ?>
