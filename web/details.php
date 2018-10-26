adf2bff1d2bd59769aeaad75b77b973a51f5745b
Vanskater362 <wag12011@byui.edu>
2018-10-25 23:29:46 -0500

    details

--- web/details.php
+++ web/details.php
@@ -21,17 +21,17 @@
 
       echo "jNameID = " . $jNameID;
 
-      $statement = $db->prepare("SELECT job_number.number, job_name.name FROM job_name INNER JOIN job_number ON job_number.name=:job_name.id");
-      //$statement->bindValue(':jNameID', $jNameID);
+      $statement = $db->prepare("SELECT job_number.number, job_name.name FROM job_name INNER JOIN job_number ON job_number.name=job_name.id WHERE job_name.id=:jNameID");
+      $statement->bindValue(':jNameID', $jNameID);
       $statement->execute();
       $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      $jNumber = $rows['number'];
-      $jName = $rows['name'];
+      $jNumber = $rows[0]['number'];
+      $jName = $rows[0]['name'];
    ?>
 
 <div class="container">
       <div class="masthead">
-        <h3 class="text-muted"><?php echo $jNumber . ' - ' . $jName . ' ';?>Details</h3>
+        <h3 class="text-muted"><?php echo $jNumber . ' ' . $jName;?> Details</h3>
        <nav>
           <ul class="nav nav-justified">
             <li class="active"><a href="index.html">Home</a></li>
