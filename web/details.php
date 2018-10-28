45009273cee13252dd23705851eb3508c36fecd9
Vanskater362 <wag12011@byui.edu>
2018-10-27 19:30:31 -0500

    took away the [0]

--- web/details.php
+++ web/details.php
@@ -40,15 +40,15 @@
       </div>
       <div class="row" style="background: lightblue">
          <div class="col-lg-4" style="backgroud: white">
-            <h2>Address</h2><hr>
+            <h3>Address</h3><hr>
             <?php
                $statement = $db->prepare("SELECT street, city, state, zip, name FROM address INNER JOIN job_number ON number_id=job_number.id WHERE job_number.name=:jNameID");
                $statement->bindValue(':jNameID', $jNameID);
                $statement->execute();
                $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-               echo '<p>' . $row[0]['street'] . '</p>';
-               echo '<p>' . $row[0]['city'] . ', ' . $row[0]['state'] . ' ' . $row[0]['zip'] . '</p>';
+               echo '<p>' . $row['street'] . '</p>';
+               echo '<p>' . $row['city'] . ', ' . $row['state'] . ' ' . $row['zip'] . '</p>';
             ?>
          </div>   
       </div>
