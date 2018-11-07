303c630ee7e65aa1086b82a6213b2643d7a7293a
Vanskater362 <wag12011@byui.edu>
2018-11-06 21:04:42 -0600

    working on login stuff

--- web/db/insert_note.php
+++ web/db/insert_note.php
@@ -16,13 +16,13 @@ $jNumberID = $rows[0]['id'];
 $query = 'INSERT INTO notes(user_id, number_id, name_id, note_text) VALUES(:userID, :number_id, :name_id, :note_text)';
 $statement = $db->prepare($query);
 
-echo $userID;
-/*$statement->bindValue(':userID', $userID, PDO::PARAM_INT)
+$statement->bindValue(':userID', $userID, PDO::PARAM_INT)
 $statement->bindValue(':number_id', $jNumberID, PDO::PARAM_INT);
 $statement->bindValue(':name_id', $name_id, PDO::PARAM_INT);
 $statement->bindValue(':note_text', $note_text, PDO::PARAM_STR);
 $statement->execute();
-$new_page = "../details.php?jNameID=$name_id";
+echo $query;
+/*$new_page = "../details.php?jNameID=$name_id";
 header("Location: $new_page");
 die();*/
 ?>
