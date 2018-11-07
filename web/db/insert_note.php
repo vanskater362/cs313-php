625cf0abaa99b3d5e4cdcfc87da27eca1eab9161
Vanskater362 <wag12011@byui.edu>
2018-11-06 20:54:11 -0600

    working on login stuff

--- web/db/insert_note.php
+++ web/db/insert_note.php
@@ -3,8 +3,8 @@ require("dbConnect.php");
 $db = get_db();
 
 $name_id = htmlspecialchars($_POST['name_id']);
-$note_text = htmlspecialchars($_POST['note_text']);
 $userID = htmlspecialchars($_POST['userID']);
+$note_text = htmlspecialchars($_POST['note_text']);
 
 $statement = $db->prepare("SELECT id FROM job_number WHERE name=:jNameID");
 $statement->bindValue(':jNameID', $name_id, PDO::PARAM_INT);
@@ -21,8 +21,8 @@ $statement->bindValue(':number_id', $jNumberID, PDO::PARAM_INT);
 $statement->bindValue(':name_id', $name_id, PDO::PARAM_INT);
 $statement->bindValue(':note_text', $note_text, PDO::PARAM_STR);
 $statement->execute();
-
-$new_page = "../details.php?jNameID=$name_id";
-header("Location: $new_page");
-die();
+echo $userID;
+//$new_page = "../details.php?jNameID=$name_id";
+//header("Location: $new_page");
+//die();
 ?>
